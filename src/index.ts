85a42aba422e186c7e5a1d58468b8c058aec7a6e
Claude Fassinou <dev.claudy@gmail.com>
2023-12-30 13:07:08 +0100

    fix:export-class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
+import { QvLocal } from "./locale/qv-local";
 import { QvConfig } from "./qv.config";
-import { Quickv, QvForm, QvInput } from "./validation";
+import { Quickv, QvBag, QvForm, QvInput } from "./validation";
 
 declare global {
   interface Window {
     QvInput: typeof QvInput;
     QvForm: typeof QvForm;
     Quickv: typeof Quickv;
+    QvBag: typeof QvBag;
+    QvLocal: typeof QvLocal;
   }
 }
 
@@ -13,6 +16,8 @@ if (typeof window !== "undefined") {
   window.QvInput = window.QvInput ?? QvInput;
   window.QvForm = window.QvForm ?? QvForm;
   window.Quickv = window.Quickv ?? Quickv;
+  window.QvBag = window.QvBag ?? QvBag;
+  window.QvLocal = window.QvLocal ?? QvLocal;
 }
 
-export { Quickv, QvForm, QvInput, QvConfig };
+export { Quickv, QvForm, QvInput, QvConfig, QvBag };
